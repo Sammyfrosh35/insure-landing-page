2f68320eacca5921663b5a80f2ced652a1601f83
Sammyfrosh35 <badejosamuel9@gmail.com>
2024-04-16 19:23:36 +0100

    new changes

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import bg from "../assets/images/bg-pattern-intro-right-desktop.svg"
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <div className='bg-Dark-Violet w-full h-full'>
+      <img src={bg} alt="bg-right" width={150} className=''/>
+      <div className='relative'>
+      <h1 className='text-3xl text-white font-DM'>Humanizing <br /> your <br /> insurace.</h1>
+      </div>
+ 
+
+    </div>
   )
 }
 
