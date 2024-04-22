7ffed914d32b4121bba0eb7cd43fe0d892d1599e
Sammyfrosh35 <badejosamuel9@gmail.com>
2024-04-22 23:07:52 +0100

    new changes

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -7,16 +7,9 @@ const Hero = () => {
     <section className='w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container bg-very-dark-violet'>
 
       <div className='relative'>
-      <p className='text-4xl text-white font-DM'>Humanizing your insurace.</p>
+      <p className='text-4xl text-white font-DM'>
+        Humanizing <br /> your insurace.</p>
       </div>
-
-
-
-      
- 
-
-
-
 
  <div>
  <button className='py-2 m-10 px-1 w-36 text-white text-[12px] border border-s-white font-karla '>      
