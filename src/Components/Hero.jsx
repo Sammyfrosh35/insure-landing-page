78b607cea739ef48f9676be4ba64950b2720f7f2
Sammyfrosh35 <badejosamuel9@gmail.com>
2024-04-23 23:37:25 +0100

    new changes

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -4,19 +4,37 @@ import heroImg from '../assets/images/image-intro-desktop.jpg'
 
 const Hero = () => {
   return (
-    <section className='w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container bg-very-dark-violet'>
+    <section className='w-full flex xl:flex-row flex-col  min-h-[70vh] gap-10 max-container bg-Dark-Violet'>
 
-      <div className='relative'>
-      <p className='text-4xl text-white font-DM'>
-        Humanizing <br /> your insurace.</p>
-      </div>
+      <div className='relative justify-center flex-col items-center'>
+      <p className='text-7xl text-white font-DM justify-start items-center relative mt-20'>
+        Humanizing <br />
+        <div className='gap-8 flex items-stretch'>
+          <span>your</span>
+          <span>insurnace.</span>
+        </div>
+    </p>
 
- <div>
- <button className='py-2 m-10 px-1 w-36 text-white text-[12px] border border-s-white font-karla '>      
+      <p className='font-karla  font-[400] text-white mt-5 text-[16px]'>
+       <span> Get your life insurance coverage easier and faster. We blend our expertise</span> <br /> 
+        <span> and technology to help you find the plan that is right for you. Ensure you </span> <br />
+          <span>and your loved ones are protected.</span>
+      </p>  
+    <button className='py-2  mt-4 w-36 text-violet-900  border border-s-white font-karla bg-white  font-semibold'>      
  <a>VIEW PLANS</a>
     </button>
- </div>
 
+
+ </div>
+        
+
+          {/* right hand side image */}
+<div className='absolute justify-end items-end flex'>
+  <img src={heroImg} alt="heroimage" 
+  height={100} />
+</div>
+
+ 
  </section>
   )
 } 
