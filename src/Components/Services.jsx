63c0d7b6abb309bb14cdcbbd95926f658518c87f
Sammyfrosh35 <badejosamuel9@gmail.com>
2024-05-12 22:38:43 +0100

    new  changes

--- src/Components/Services.jsx
+++ src/Components/Services.jsx
@@ -81,3 +81,49 @@ const Services = () => {
 }
 
 export default Services
+
+
+// import React from 'react';
+// import snappy from "../assets/icons/icon-snappy-process.svg";
+// import affordable from  "../assets/icons/icon-affordable-prices.svg";
+// import people from  "../assets/icons/icon-people-first.svg";
+
+// const Services = () => {
+//   return (
+//     <section className="max-container flex flex-col items-center gap-12 md:flex-row md:justify-center md:gap-9">
+
+//       <div className="text-center md:text-left">
+//         <hr className="mb-9 w-20 border-[1px]" />
+//         <p className="font-DM font- text-4xl text-Dark-Violet">We're different</p>
+
+//         <div className="flex flex-col md:flex-row md:gap-10 mt-16">
+//           <div className="flex-1 md:w-[350px] w-full rounded-[20px] mb-10 md:mb-0 flex flex-col items-center">
+//             <div className="w-11 h-11 flex justify-center items-center rounded-full bg-Grayish-Blue">
+//               <img src={snappy} alt="icon" width={50} height={30} />
+//             </div>
+//             <h3 className="font-semibold font-DM text-Dark-Violet text-2xl mt-3">Snappy Process</h3>
+//             <p className="font-karla font-[16px] text-dark-grayish-violet leading-normal mt-3">Our application process can be completed in minutes, not hours. Don't get stuck filling in tedious forms</p>
+//           </div>
+
+//           <div className="flex-1 md:w-[350px] w-full rounded-[20px] mb-10 md:mb-0 flex flex-col items-center">
+//             <div className="w-11 h-11 flex justify-center items-center rounded-full bg-Grayish-Blue">
+//               <img src={affordable} alt="icon" width={50} height={30} />
+//             </div>
+//             <h3 className="font-semibold font-DM text-Dark-Violet text-2xl mt-3">Affordable Prices</h3>
+//             <p className="font-karla font-[16px] text-dark-grayish-violet leading-normal mt-3">We won't want you worrying about high monthly costs. Our prices may be low, but we still offer the best coverage possible.</p>
+//           </div>
+
+//           <div className="flex-1 md:w-[350px] w-full rounded-[20px] flex flex-col items-center">
+//             <div className="w-11 h-11 flex justify-center items-center rounded-full bg-Grayish-Blue">
+//               <img src={people} alt="icon" width={50} height={30} />
+//             </div>
+//             <h3 className="font-semibold font-DM text-Dark-Violet text-2xl mt-3">People First</h3>
+//             <p className="font-karla font-[16px] text-dark-grayish-violet leading-normal mt-3">Our plans aren't full of conditions and clauses to prevent payouts. We make sure you're covered when you need it.</p>
+//           </div>
+//         </div>
+//       </div>
+//     </section>
+//   );
+// };
+
+// export default Services;
--- src/Components/Work.jsx
+++ src/Components/Work.jsx
@@ -1,26 +1,47 @@
-import React from 'react'
+// import React from 'react'
 
 
-const Work = () => {
-  return (
-  <section className=''>
-   <div className='bg-Dark-Violet w-[100] h-44 flex items-center relative p-6 bg-work-pattern bg-cover bg-no-repeat flex-row '>
-<p className='text-4xl font-DM  font-normal text-white '>Find out more <br /> about how we work
-</p>
+// const Work = () => {
+//   return (
+//   <section className=''>
+//    <div className='bg-Dark-Violet w-[100] h-44 flex items-center relative p-6 bg-work-pattern bg-cover bg-no-repeat flex-row '>
+// <p className='text-4xl font-DM  font-normal text-white '>Find out more <br /> about how we work
+// </p>
 
   
-<div className='absolute right-14'> 
-<button className='border-[1px] border-white font-karla text-white font-medium p-3 justify-end ml-10 hover:text-very-dark-violet hover:bg-white'> HOW WE WORK</button>
-</div>
+// <div className='absolute right-14'> 
+// <button className='border-[1px] border-white font-karla text-white font-medium p-3 justify-end ml-10 hover:text-very-dark-violet hover:bg-white'> HOW WE WORK</button>
+// </div>
 
 
 
-   </div>
+//    </div>
  
 
 
-  </section>
-  )
-}
+//   </section>
+//   )
+// }
 
-export default Work
+// export default Work
+
+import React from 'react';
+
+const Work = () => {
+  return (
+    <section className=''>
+      <div className='bg-Dark-Violet h-[15rem] sm:h-[10rem] flex items-center justify-center relative p-6 bg-work-pattern bg-cover bg-no-repeat'>
+        <p className='text-4xl sm:text-2xl font-DM font-normal text-white text-center'>
+          Find out more <br /> about how we work
+        </p>
+        <div className='absolute bottom-4 sm:bottom-auto right-4 sm:right-auto left-4 sm:left-auto'>
+          <button className='border-[1px] border-white font-karla text-white font-medium p-3 sm:p-2 hover:text-very-dark-violet hover:bg-white'>
+            HOW WE WORK
+          </button>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Work;
